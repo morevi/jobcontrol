be9e7ef4b9003bd8506447f6538dfda919fb090f
Antonio Caño Lopez <e.antoniocalo18@go.ugr.es>
2021-11-29 15:44:55 +0100

    Representación clase Horario y Proyecto
    Representación de las Clases Horario y Proyecto para avanzar en M0.
    - Clase Horario: 
      - Contiene los parámetros id, usuario_relacionado, horas_semanales y dias_trabajados
      - Contiene los métodos: horas_trabajadas, add_dia, get_dias_trabajados, get_horas_restantes y to_CSV.
    Los métodos get_horas_restantes y to_CSV permiten: closes morevi#29 y closes morevi#30 respectivamente
    
    -Clase Proyecto: 
       - Contiene los parámetros id, nombre, planificación y usuarios_asignados
       -  Contiene los métodos: is_finalizado y horas_restantes
    
    Con la creación de dichas clases se ha determinado la necesidad de representar un Usuario y se ha creado el issue correspondiente

---
+++ src/horario.py
@@ -0,0 +1,58 @@
+#----------------------------------------------------------------------------
+# Este archivo producto.py contiene la implementaciÃ³n de la clase Producto.
+#----------------------------------------------------------------------------
+# @author: Antonio Caño @eantoniocalo18 
+# @created_date: 29/11/2021
+# @version: '2.0'
+# ---------------------------------------------------------------------------
+
+
+class Horario:
+    """
+    Clase utilizada para representar un Horario.
+    ...
+    Atributos
+    ----------
+   
+    Publicos
+    id: str
+        uuid o id que identifica cada horario único
+    usuario_relacionado : str
+        uuid o id que identifica el usuario asociado al 
+    horas_semanales: int
+        entero que determinará las horas que tiene que trabajar un usuario en una semana
+    dias_trabajados : Dia []
+        array de objetos dia que contendrá el horario
+    Métodos
+    -------
+        horas_trabajadas():
+            "Método que devolverá las horas trabajadas por el usuario en este horario"
+        add_dia():
+            "Método que añadirá un nuevo día al vector de días trabajados"
+        get_dias_trabajados():
+            "Método que devolverá el vector de días que se han trabajado"
+        get_horas_restantes():
+            "Método que devolverá las horas que le quedan por trabajar al usuario en la semana actual"
+        to_CSV():
+            "Método que pasa los datos referentes al horario al formato .csv"
+    """
+    def __init__(self, id, usuario_relacionado,horas_semanales, dias_trabajados):
+        self.id = id
+        self.nombre = usuario_relacionado
+        self.horas_semanales = horas_semanales
+        self.dias_trabajados = dias_trabajados
+
+    def horas_trabajadas():
+        "Método que devolverá las horas trabajadas por el usuario en este horario"
+        pass
+    def  add_dia():
+        "Método que añadirá un nuevo día al vector de días trabajados"
+        pass
+    def  get_dias_trabajados():
+        "Método que devolverá el vector de días que se han trabajado"
+        pass
+    def  get_horas_restantes():
+        "Método que devolverá las horas que le quedan por trabajar al usuario en la semana actual"
+        pass
+    
+    
