6584e38160212aa5c36757e1aa2be6436f8eb129
eantoniocalo18 <e.antoniocalo18@go.ugr.es>
2021-12-08 18:36:46 +0100

    Closes #29, Avanza #41 Añade método to_CSV()
    Se ha creado un método to_CSV() que permitirá al usuario exportar los datos referentes
    a su horario a dicho formato. Esto le permitirá ahorrar tiempo puesto que antes
    tenía que hacerlo manualmente, solucionando así #29.
    Además, avanza en M0

--- src/Horario.py
+++ src/Horario.py
@@ -23,12 +23,14 @@ class Horario:
         entero que determinará las horas que tiene que trabajar un usuario en una semana
     horas_trabajadas : int
 	entero que lleva la sumatoria de las horas que ha ido trabajando un usuario
-    
+
     Métodos
     -------
         horas_restantes():
             "Método que devolverá la diferencia entre horas_semanales y horas_trabajadas
-    """
+	to_CSV(): 
+	    "Método que permitirá exportar a formato CSV los datos referentes a un horario" 
+ """
     def __init__(self, id, usuario_relacionado,horas_semanales, horas_trabajadas):
         self.id = id
         self.nombre = usuario_relacionado
@@ -39,3 +41,6 @@ class Horario:
 	" Método que devolverá la diferencia entre horas_semanales y horas_trabajadas"
 	pass
 
+    def to_CSV():
+        " Método que devolverá las horas trabajadas por el usuario en este horario"
+	pass
