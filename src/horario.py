0cec80c3914ac1d83bd065a25f861c45d0425226
morevi <morevi@correo.ugr.es>
2021-12-12 17:09:33 +0100

    Reformat comments to PEP
    Closes #49

--- src/horario.py
+++ src/horario.py
@@ -1,41 +1,16 @@
-#----------------------------------------------------------------------------
-# Este archivo producto.py contiene la implementación de la clase Horario.
-#----------------------------------------------------------------------------
-# @author: Antonio Caño @eantoniocalo18 
-# @created_date: 29/11/2021
-# @version: '2.0'
-# ---------------------------------------------------------------------------
-
+'''Horario.py'''
 
 class Horario:
-    """
-    Clase utilizada para representar un Horario.
-    ...
-    Atributos
-    ----------
-   
-    Publicos
-    fecha_inicio: Date
-        fecha en la que comienza el horario
-    usuario_relacionado : str
-        uuid o id que identifica el usuario asociado al
-    horas_trabajadas : int
-	entero que lleva la sumatoria de las horas que ha ido trabajando un usuario
+    '''Represent a weekly timetable'''
 
-    Métodos
-    -------
-        get_horas_trabajadas():
-            " Método que devolverá las horas_trabajadas por el usuario"
-	to_CSV(): 
-	    "Método que permitirá exportar a formato CSV los datos referentes a un horario" 
- """
-    def __init__(self, fecha_inicio, usuario_relacionado, horas_trabajadas):
+    def __init__(self, fecha_inicio, usuario_relacionado, horas_trabajadas: float):
+        '''Initialize the object'''
         self.fecha_inicio = fecha_inicio
         self.nombre = usuario_relacionado
         self.horas_trabajadas = horas_trabajadas
 
-    def get_horas_trabajadas():
-        " Método que devolverá las horas_trabajadas por el usuario"
+    def get_horas_trabajadas() -> float:
+        '''Counts the number of hours worked'''
 
-    def to_CSV():
-        " Método que devolverá las horas trabajadas por el usuario en este horario"
+    def to_CSV() -> str:
+        '''Export this timetable on a human readable format'''
