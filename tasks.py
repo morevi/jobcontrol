3b83765aefd8acc1fdab54d8ca414aa103313d5a
morevi <1998morevi@gmail.com>
2021-12-16 12:12:28 +0100

    Add tasks for building, pushing, and running #56
    This helps doing these tasks locally

--- tasks.py
+++ tasks.py
@@ -9,3 +9,18 @@ def check(ctx):
 def test(ctx):
     '''Run tests with ward'''
     run('ward', pty=True)
+
+@task
+def docker_build(ctx):
+    '''Build image'''
+    run('docker build -t morevi/jobcontrol .')
+
+@task
+def docker_push(ctx):
+    '''Push container'''
+    run('docker push morevi/jobcontrol', pty=True)
+
+@task
+def docker_run(ctx):
+    '''Run tests with docker and ward'''
+    run('docker run -t -v `pwd`:/app/test morevi/jobcontrol', pty=True)
